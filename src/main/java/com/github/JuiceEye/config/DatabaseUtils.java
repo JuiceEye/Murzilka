30a58dc25359984509988f048ee9aea104efa298
Farukh <cool.faruh42@gmail.com>
2020-11-16 19:28:25 +0600

    Added database, collections are not in src anymore

--- src/main/java/com/github/JuiceEye/config/DatabaseUtils.java
+++ src/main/java/com/github/JuiceEye/config/DatabaseUtils.java
@@ -9,6 +9,8 @@ import com.j256.ormlite.table.TableUtils;
 import java.sql.SQLException;
 
 public class DatabaseUtils {
+
+
     public static String JDBC_CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\Администратор\\Desktop\\database\\murzilkaDatabase.sqlite";
 
     public static ConnectionSource CONNECTION_SOURCE;
